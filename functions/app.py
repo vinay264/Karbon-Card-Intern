040282435bd6a8dea1966b11dab40638e6c911c0
vinay264 <109301886+vinay264@users.noreply.github.com>
2024-01-23 20:16:31 +0530

    Update app.py

--- functions/app.py
+++ functions/app.py
@@ -1,1 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+from model import probe_model_5l_profit
+import json
+app = Flask(__name__, template_folder='app/templates')
 
+
+@app.route('/')
+def index():
+    return render_template('page1.html')
+
+
+@app.route('/process_data', methods=['POST'])
+def process_data():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+
+    content = file.read().decode('utf-8')
+    data = json.loads(content)
+    results = probe_model_5l_profit(data["data"])
+
+    return render_template('page2.html', results=results)
+    
+# if __name__ == '__main__':
+#     app.run(debug=True)
